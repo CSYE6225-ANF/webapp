90bdd83de8c121a8d3ad462cdfed5c67b5e6b898
angferna <angelnfernandes@gmail.com>
2024-10-17 01:02:45 -0400

    Update yaml files, scripts and packer file

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -13,6 +13,10 @@ sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_US
 # Ensure PostgreSQL listens on all IP addresses
 sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/*/main/postgresql.conf
 
+# Change 'peer' to 'md5' for local connections in pg_hba.conf
+sudo sed -ri "s/^(local\s+all\s+postgres\s+)peer/\1md5/" /etc/postgresql/*/main/pg_hba.conf
+sudo sed -ri "s/^(local\s+all\s+all\s+)peer/\1md5/" /etc/postgresql/*/main/pg_hba.conf
+
 # Configure PostgreSQL to allow password authentication from all IPs
 echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf
 
