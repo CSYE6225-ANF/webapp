c705f27bbd84c6b1e27778140b6154b11ea17154
Angel N. Fernandes <102985556+angferna@users.noreply.github.com>
2024-10-19 17:00:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
-const healthRoutes = require('./routes/health.route');
-const userRoutes = require('./routes/user.route');
+const indexRoutes = require('./routes/index');
 
 // Middleware to parse JSON
 app.use(express.json());
 
-// Use health routes
-app.use('/healthz', healthRoutes);
-
-// Use user routes
-app.use('/v1/user', userRoutes);
+// Use the centralized routes defined in 'index.js'
+app.use('/', indexRoutes);
 
 module.exports = app;  // Export the app
