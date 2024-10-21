a69f537079811da1773e5d4755057ab9f896977c
Angel N. Fernandes <102985556+angferna@users.noreply.github.com>
2024-10-21 16:34:37 -0400

    Updated config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,20 @@
-require("dotenv").config();
-module.exports = {
-    DB_HOST: process.env.DB_HOST,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_NAME: process.env.DB_NAME,
-    DB_PORT: process.env.DB_PORT || 5432,
-    PORT: process.env.PORT,
-    HOSTNAME: process.env.HOSTNAME,
-    dialect: "postgres",
+const config = {
+    production: {
+        username: process.env.DB_USER || 'csye6225',  // From Terraform
+        password: process.env.DB_PASSWORD || 'csye6225', // From Terraform
+        database: process.env.DB_NAME || 'csye6225',  // Set your DB name (same as Terraform)
+        host: process.env.DB_HOST,                    // RDS endpoint from Terraform
+        dialect: 'postgres',
+        port: process.env.DB_PORT || 5432,            // Use port 3306 for MySQL/MariaDB
+    },
+    development: {
+        username: 'postgres',  // Local DB user for development/testing
+        password: 'postgres',  // Local DB password
+        database: 'postgres',  // Local database name for tests
+        host: 'localhost',    // Localhost for development
+        dialect: 'postgres',
+        port: 5432,
+    },
 };
+
+module.exports = config;
