a2926416f699c33151f0b7cdfe706f7bb2877914
Angel N. Fernandes <102985556+angferna@users.noreply.github.com>
2024-10-21 16:46:44 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,14 +1,9 @@
 const { Sequelize } = require("sequelize");
 const dbConfig = require("../config/config.js");
 
-// Environment Setup: production or development
-const environment = process.env.NODE_ENV || 'development';
-console.log(`Running in ${environment} mode`);
-const dbConfig = config[environment];
-
 // Initialize Sequelize with PostgreSQL settings
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-    host: dbConfig.host,
+const sequelize = new Sequelize(dbConfig.DB_NAME, dbConfig.DB_USER, dbConfig.DB_PASSWORD, {
+    host: dbConfig.DB_HOST,
     dialect: dbConfig.dialect,
     port: dbConfig.port, 
     logging: false, // Change to true for debugging
