6ab92c0347c7b839acd6a7bdab3c09f477819656
Angel N. Fernandes <102985556+angferna@users.noreply.github.com>
2024-10-29 20:18:14 -0400

    Update statsd.js

--- utils/statsd.js
+++ utils/statsd.js
@@ -1,9 +1,9 @@
 var Client = require('node-statsd');
-const client = new Client('0.0.0.0', 8125);
+const client = new Client("localhost", 8125);
 
 // Add a close method for Jest cleanup
-client.close = function() {
-    this.socket && this.socket.close();
-};
+// client.close = function() {
+//     this.socket && this.socket.close();
+// };
 
 module.exports = client;
